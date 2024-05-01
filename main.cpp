284f123c3b407d1ba59b84340fce91459a2fd511
wba6 <bradenaey1@gmail.com>
2024-04-30 21:53:13 -0400

    A4B attempt

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <list>
 #include <iostream>
+#include <cmath>
 
-void backPropigate(std::list<double>::iterator it, std::list<double>& list);
+void backPropigate(std::list<double>::iterator it, std::list<double>& list, double bigram, double bias, double slope);
 
 int main() {
     //list with 5 nodes
@@ -11,7 +12,7 @@ int main() {
     list.emplace_front();
     list.emplace_front();
     list.emplace_front();
-    backPropigate(list.end(),list);
+    backPropigate(list.end(),list, 1,1,1);
 
     for(auto it:list){
         std::cout << it << " ";
@@ -22,15 +23,15 @@ int main() {
     return 0;
 };
 
-void backPropigate(std::list<double>::iterator it, std::list<double>& list) {
+void backPropigate(std::list<double>::iterator it, std::list<double>& list, double bigram, double bias, double slope) {
     if (it != list.begin() && it != list.end()) {
-        *it = *(std::next(it)) * 0.5;
-        backPropigate(std::prev(it), list);
+        *it = slope = *(std::next(it)) * std::pow(10,-bigram);
+        backPropigate(std::prev(it), list, bigram, slope, slope);
     } else if (it == list.end()) {
         it = std::prev(it);
-        *it = 0.5;
-        backPropigate(std::prev(it), list);
+        *it = slope = std::pow(10,-bigram);
+        backPropigate(std::prev(it), list, bigram, slope, slope);
     } else {
-        *it = *(std::next(it)) * 0.5;
+        *it = abs(std::tanh(bigram)-bias);
     }
 }
